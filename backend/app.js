3be447ce76cd247abf061d80718419f5af081dd1
Sawadogo Issa <76668193+BenIssa1@users.noreply.github.com>
2023-05-23 23:14:54 +0000

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+app.use(cors({
+    origin: 'https://creative-frangollo-8407ae.netlify.app/'
+}));
+
 const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
@@ -30,31 +34,5 @@ app.use("/api/v1", commentTaleRoute);
 // Middleware for Errors
 app.use(errorMiddleware);
 
-app.use(function(req, res, next) {
-    var oneof = false;
-    if(req.headers.origin) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-method']) {
-        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-headers']) {
-        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-        oneof = true;
-    }
-    if(oneof) {
-        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    }
-
-    // intercept OPTIONS method
-    if (oneof && req.method == 'OPTIONS') {
-        res.send(200);
-    }
-    else {
-        next();
-    }
-});
 
 module.exports = app;
