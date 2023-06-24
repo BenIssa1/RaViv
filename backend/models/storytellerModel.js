2b9e7de5f3a8e864d2a55f8a820e46d76c750a47
Test <test@gmail.com>
2023-06-24 11:57:17 +0000

    add column to storyteller

--- backend/models/storytellerModel.js
+++ backend/models/storytellerModel.js
@@ -3,6 +3,10 @@
 const mongoose = require("mongoose");
 
 const storytellerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   phone: {
     type: String,
     required: true,
